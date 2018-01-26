ab486a15c4109a09a5d447efe224bf9ae68cad1e
michto <michto@poczta.onet.pl>
2018-01-26 02:40:39 +0100

    panel dane wejsciowe + constants

---
+++ src/model/rowProbability.ts
@@ -0,0 +1,15 @@
+import {Constants} from "utils/constants"
+
+export class ProbabilityRow {
+  label: string;
+  nuclMap : { [key:string]:number; } = {};
+
+  constructor(label:string, valueA: number, valueG: number, valueC: number, valueT: number, valueBreak:number) {
+    this.label = label;
+    this.nuclMap[Constants.A_NUCL] = valueA;
+    this.nuclMap[Constants.C_NUCL] = valueC;
+    this.nuclMap[Constants.G_NUCL] = valueG;
+    this.nuclMap[Constants.T_NUCL] = valueT;
+    this.nuclMap[Constants.BREAK_NUCL] = valueBreak;
+  }
+}
